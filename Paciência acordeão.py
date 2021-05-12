081a92745ead360974e0e92fd139bf6a977f19e1
juliacfigueiredo <julia.figueiredo2003@icloud.com>
2021-05-11 21:12:08 -0300

    primeiro commit

---
+++ Paciência acordeão.py
@@ -0,0 +1,113 @@
+print('Seja bem-vindo(a) ao jogo de Paciência Acordeão! O objetivo deste jogo é colocar todas as cartas em uma mesma pilha.')
+
+print('Existem apenas dois movimentos possíveis:')
+
+print('1. Empilhar uma carta sobre a carta imediatamente anterior;')
+print('2. Empilhar uma carta sobre a terceira carta anterior.')
+
+print('Para que um movimento possa ser realizado basta que uma das duas condições abaixo seja atendida:')
+
+print('1. As duas cartas possuem o mesmo valor ou')
+print('2. As duas cartas possuem o mesmo naipe.')
+
+print('Desde que alguma das condições acima seja satisfeita, qualquer carta pode ser movimentada.')
+
+inicio = int(input("\nAPERTE: \n[ENTER] PARA JOGAR "))
+
+As = ['A♠', 'A♥', 'A♦', 'A♣']
+Dois = ['2♠', '2♥', '2♦', '2♣']
+Tres =  ['3♠', '3♥', '3♦', '3♣']
+Quatro = ['4♠', '4♥', '4♦', '4♣']
+Cinco = ['5♠', '5♥', '5♦', '5♣']
+Seis = ['6♠', '6♥', '6♦', '6♣']
+Sete = ['7♠', '7♥', '7♦', '7♣']
+Oito = ['8♠', '8♥', '8♦', '8♣']
+Nove = ['9♠', '9♥', '9♦', '9♣']
+Dez = ['10♠', '10♥', '10♦', '10♣']
+Valete = ['J♠', 'J♥', 'J♦', 'J♣']
+Dama = ['Q♠', 'Q♥', 'Q♦', 'Q♣']
+Rei = ['K♠', 'K♥', 'K♦', 'K♣']
+
+def cria_baralho():
+    baralho = ['A♠', 'A♥', 'A♦', 'A♣','2♠', '2♥', '2♦', '2♣', '3♠', '3♥', '3♦', '3♣', '4♠', '4♥', '4♦', '4♣', '5♠', '5♥', '5♦', '5♣', '6♠', '6♥', '6♦', '6♣', '7♠', '7♥', '7♦', '7♣', '8♠', '8♥', '8♦', '8♣', '9♠', '9♥', '9♦', '9♣', '10♠', '10♥', '10♦', '10♣', 'J♠', 'J♥', 'J♦', 'J♣', 'Q♠', 'Q♥', 'Q♦', 'Q♣', 'K♠', 'K♥', 'K♦', 'K♣']
+    return baralho
+
+print('Escolha uma carta (digite um número entre 1 e 49):')
+
+def escolha(carta):
+    if carta >= 1 and <= 49
+
+def extrai_naipe(carta):
+    resposta = carta[1]
+    if carta[0] == '1':
+        resposta = carta[2]
+    return resposta
+
+def extrai_valor(carta):
+    resposta = carta[0]
+    if carta[0] == '1':
+        resposta = '10'
+    return resposta
+
+def lista_movimentos_possiveis(baralho, posicao):
+    carta = baralho[posicao]
+    naipe = extrai_naipe(carta)
+    valor = extrai_valor(carta)
+    movimentos = []
+    if posicao > 2:
+        movimento3 = baralho[posicao - 3]
+        naipe3 = extrai_naipe(movimento3)
+        valor3 = extrai_valor(movimento3)
+        if naipe == naipe3 or valor == valor3:
+            movimentos.append(3)
+        
+        movimento1 =  baralho[posicao - 1]
+        naipe1 = extrai_naipe(movimento1)
+        valor1 = extrai_valor(movimento1)
+        if naipe == naipe1 or valor == valor1:
+            movimentos.append(1)
+
+    elif posicao > 0:
+        movimento1 =  baralho[posicao - 1]
+        naipe1 = extrai_naipe(movimento1)
+        valor1 = extrai_valor(movimento1)
+        if naipe == naipe1 or valor == valor1:
+            movimentos.append(1)
+    else:
+        movimentos = []
+    return movimentos
+
+def empilha(lista_baralho, posicao_origem, destino):
+    posicao_origem1 = lista_baralho[posicao_origem]
+    lista_baralho[destino] = posicao_origem1
+    del lista_baralho[posicao_origem]
+    return lista_baralho
+
+def possui_movimentos_possiveis(baralho):
+    for i in range (len(baralho)):
+        carta = baralho[i]
+        valor = extrai_valor(carta)
+        naipe = extrai_naipe(carta)
+        if i > 2:
+            carta3 = baralho[i-3]
+            valor3 = extrai_valor(carta3)
+            naipe3 = extrai_naipe(carta3)
+            if valor == valor3 or naipe == naipe3:
+                resposta = True
+            
+            carta1 = baralho[i-1]
+            valor1 = extrai_valor(carta1)
+            naipe1 = extrai_naipe(carta1)
+            if valor == valor1 or naipe == naipe1:
+                resposta = True
+        elif i > 0:
+            carta1 = baralho[i-1]
+            valor1 = extrai_valor(carta1)
+            naipe1 = extrai_naipe(carta1)
+            if valor == valor1 or naipe == naipe1:
+                resposta = True 
+        else:
+            resposta = False      
+    
+    return resposta
+
